09d2ae0f6ae9104bd73da277f53115dc111c4256
oprinter <ozgur314159@gmail.com>
2023-01-05 23:13:45 +0300

    05.01.2023

---
+++ src/test/java/tests/day15/C04_NegativeLoginTesti.java
@@ -0,0 +1,54 @@
+package tests.day15;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+
+public class C04_NegativeLoginTesti {
+    // 3 test metodu olusturun.
+// 1. de yanlis e mail dogru sifre
+//2.de dogru email yanlis sifre
+// 3. yanlis email yanlis sifre ile giris yapmayi deneyin.
+// giris yapilamadigini test edin
+
+    QualitydemyPage qualitydemyPage=new QualitydemyPage();
+
+    @Test
+    public void yanlisEmailTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("mehmet@abc.com");
+        qualitydemyPage.passwordKutusu.sendKeys("31488081");
+        qualitydemyPage.loginButonu.click();
+                Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+                Driver.closeDriver();
+
+    }
+    @Test
+    public void yanlisPasswordTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("user_1106147@login.com");
+        qualitydemyPage.passwordKutusu.sendKeys("11449081");
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+        Driver.closeDriver();
+
+    }
+
+    @Test
+    public void yanlisEmailPasswordTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.kullaniciEmailKutusu.sendKeys("mehmet@abc.com");
+        qualitydemyPage.passwordKutusu.sendKeys("11449081");
+        qualitydemyPage.loginButonu.click();
+
+        Assert.assertTrue(qualitydemyPage.kullaniciEmailKutusu.isDisplayed());
+        Driver.closeDriver();
+
+    }
+
+}
