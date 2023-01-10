7e3d0a983aa19055b111c854c6138515c9330fbe
ÖMER <ozgur314159@gmail.com>
2023-01-10 22:19:06 +0300

    10.01.2023

---
+++ src/test/java/tests/day19_crossBrowser/C02_WiseQuarterTest.java
@@ -0,0 +1,25 @@
+package tests.day19_crossBrowser;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ReusableMethods;
+import utilities.TestBaseCross;
+
+public class C02_WiseQuarterTest extends TestBaseCross {
+
+
+    @Test
+    public void test01() throws InterruptedException {
+
+        // WiseQuarter anasayfaya gidelim
+        driver.get("https://www.wisequarter.com");
+
+        // wise quarter sitesine gittigimizi test edelim
+        String actualUrl= driver.getCurrentUrl();
+        String expectedKelime="wisequarter";
+
+        Assert.assertTrue(actualUrl.contains(expectedKelime));
+
+
+    }
+}
